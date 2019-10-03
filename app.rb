265838e596ccf230abab4093e9b4f9e570353a1a
Randy Taylor <randyisbusy@gmail.com>
2019-10-03 16:11:31 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,11 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do
-    "Welcome to the jungle!!!! We got fun and games we got everything you need"
+    <<-YOO
+    Welcome to the jungle!
+    We got fun and games
+    We got everthing you need
+        YOO
   end
 
 end
